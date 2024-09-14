ef46e2707ac85b72f6dab7b2e2f6df55a82687f0
Toonu <toonu4@icloud.com>
2024-09-14 17:28:54 +0000

    Sync Test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 
 function App() {  
   
+  fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data => {
+    const userIp = data.ip;  // Get the IP address
+    console.log("Your IP address is: " + userIp);
+  });
 
-  useEffect(() => {    setTimeout(() => {      window.location.href = 'https://www.toonudesign.tech/';    }, 100);  }, []);
+  useEffect(() => {    setTimeout(() => {      window.location.href = 'https://www.toonudesign.tech/';    }, 10000);  }, []);
   return <p>Redirecting in 100 milliseconds... (the website is in construction phase and will redirect for the time being)</p>;
 }
 
